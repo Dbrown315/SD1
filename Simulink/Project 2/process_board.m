a790fe3aafa5748eb94069ec65dcd652be3d4165
David Brown <dmb5@clemson.edu>
2026-02-17 12:10:58 -0500

    initialize project 2 branch

---
+++ Simulink/Project 2/process_board.m
@@ -0,0 +1,17 @@
+%{
+Specifically for project 2, the processing engine must be able to automatically identify the number, 
+positions (pixel coords), shapes, and colors of spots on the game board.
+
+Specifically for the demo we are required to have a game state structure
+and various outputs.
+The struct should contain:
+Color and location of each identified colored sticker (space)
+Location (pixel coords) of the centroid of each identified space
+Images used for analysis:
+- Original (background) Image
+- Current image
+- Differential Image
+- Difference after noise removal and enhancement of foreground objects
+- Additional outputs from processing
+%}
+
---
+++ Simulink/Project 2/vision_testing.m
@@ -0,0 +1,26 @@
+%% Test file for mess with functions and ideas before implementing them in the main procesisng file/simulink
+close all
+clc
+clear
+clear('cam')
+%% Check Camera List (may have to install MATLAB package)
+cam_list = webcamlist
+%% Assign webcam to be used
+% Pick an index from the camera list above
+cam_name = cam_list{2}
+%% Check webcam properties
+cam = webcam(cam_name)
+%% Preview cam
+preview(cam)
+%% Close Preview
+closePreview(cam)
+%% Snapshot 1
+img = snapshot(cam);
+figure();
+imshow(img)
+title('Snapshot 1')
+%% Snapshot 2
+img2 = snapshot(cam);
+figure();
+imshow(img2)
+title('Snapshot 2')
